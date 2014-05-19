ab4b620806c2e6cff716fa9cc5da0aa59e04a610
Thomas P <tpxp@live.fr>
2014-05-19 18:18:33 +0200

    Gracefully closing MySQL connections when asked to leave

--- www/server.js
+++ www/server.js
@@ -5,13 +5,22 @@ var swig = require('swig');
 var mysql = require('mysql');
 var crypto = require('crypto');
 
-var config, db; //Define these variables out of any function so that they are global and accessible by everyone
+// Global tools
+var config, db;
+
+console.info(' *******************');
+console.info(' * Welcome to CPX! *');
+console.info(' *******************')
+console.info('');
+console.info(' * Loading Configuration...');
 
 // Getting the configuration
 fs.readFile(__dirname + '/../config.json', {encoding: 'utf-8'}, function(err, data){
 	if(err)
 		throw 'Unable to read configuration file : ' + err;
 	config = JSON.parse(data);
+	
+	console.log('Successfully loaded configuration !');
 	
 	// Set a Database Connections Pool.
 	db = mysql.createPool({
@@ -26,13 +35,16 @@ fs.readFile(__dirname + '/../config.json', {encoding: 'utf-8'}, function(err, da
 	db.on('connection', function(connection){
 		connection.query('SET NAMES UTF8', function(err, rows){ // Use UTF-8 encoding
 			if(err)
-				console.log('Could not set Database encoding as UTF-8 : ' + err);
+				console.warn('Could not set Database encoding as UTF-8 : ' + err);
 			connection.query('USE ' + config.db_name, function(err, rows){ // Use our database
 				if(err)
-					console.log('Could not use CPX Database : ' + err);
+					console.warn('Could not use CPX Database : ' + err);
 			}); 
 		});
 	});
+	
+	console.log('');
+	console.log(' * Testing database connection...');
 	
 	// Let's see if the database works !
 	db.getConnection(function(err, connection){
@@ -51,8 +63,26 @@ fs.readFile(__dirname + '/../config.json', {encoding: 'utf-8'}, function(err, da
 			if(rows[0].answer != 2)
 				throw 'The Database did not reply correctly to our test request.';
 			
+			console.log('It seems database is working! Launching webserver...');
 			// Time to start the WebServer!
 			app.listen(config.www_port);
+			console.log('Everything shoud be OK now, have fun!');
+			
+			// Now that the connection is established, we must ensure it ends gracefully
+			process.on('SIGINT', function(){
+				console.log('');
+				console.log(' * Got interrupt signal, closing database connections...');
+				db.end(function(err){
+					if(err)
+						console.warn('Something went wrong trying to close database connections : ' + err);
+					else
+						console.info('Connections closed!');
+					console.info('');
+					console.info('All done! Thank your for using CPX!');
+					// Now we can leave!
+					process.exit(0);
+				});
+			});
 		});
 	});
 });
@@ -93,4 +123,3 @@ function hashpw(password){
 	// Return hexdecimal hash
 	return sha1.digest('hex');
 }
-console.log(hashpw('a').length);
